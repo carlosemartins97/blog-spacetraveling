bff3b464e15b02c040f21487a2776a189cbbf002
Carlos Martins <carlosemartins97@gmail.com>
2021-03-25 12:51:40 -0300

    fix: logo redirect to home page /

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import styles from './header.module.scss';
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className={styles.container}>
       <div className={styles.headerContent}>
-        <img src='/logo.svg' alt="logo"/>
+        <Link href="/">
+          <a><img src='/logo.svg' alt="logo"/></a>
+        </Link>
       </div>
     </header>
   )
