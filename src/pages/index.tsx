1ff21e9ac9d7810258598db70e059ff0a1840e28
Carlos Martins <carlosemartins97@gmail.com>
2021-03-25 14:29:02 -0300

    feat: create home page posts structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+import {FiCalendar, FiUser} from 'react-icons/fi';
+
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -29,7 +31,27 @@ export default function Home() {
   return (
     <>
       <Header />
-      <h1>teste</h1>
+      <main className={styles.container}>
+        <div className={styles.homeContent}>
+          <a href="#">
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.postInfo}>
+              <time><FiCalendar />15 Mar 2021</time>
+              <span><FiUser />Carlos Martins</span>
+            </div>
+          </a>
+
+          <a href="#">
+            <h1>Criando um app CRA do zero</h1>
+            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App.</p>
+            <div className={styles.postInfo}>
+              <time><FiCalendar />19 Abr 2021</time>
+              <span><FiUser />Danilo Vieira</span>
+            </div>
+          </a>
+        </div>
+      </main>
     </>
   )
 }
